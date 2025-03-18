ff87977b357c8da25dd8c968fccf657a5148f433
elizabeth zenah <yienyazenha@gmail.com>
2025-03-18 10:22:56 +0300

    added python files

---
+++ conditions.py
@@ -0,0 +1,97 @@
+# conditional elements
+from multiprocessing.managers import view_type
+from operator import truediv
+
+x=10
+y=50
+# variable == store a variable
+print(x)
+my_city ="Nairobi"
+# camel casing
+myOldestcar ="suzuki"
+name ="zenah"
+
+
+# types of data
+#numbers
+height=2 #integer
+weight=30.45 #float
+#strings
+full_name="Zenah Yienya"
+distance= 45
+
+#boolean
+is_listed=True
+is_disabled=False
+
+#conditional statements
+
+password= "123456"
+db_password="654321"
+
+
+
+if password== db_password:
+
+ print("success")
+
+else:
+
+ print("failure")
+
+
+ score= 64
+if score >= 90:
+
+      print("A")
+
+elif score >= 80 and score >= 89:
+    print("A-")
+
+elif score >= 70 and score >= 79:
+    print("B+")
+
+
+elif score >= 60 and score >= 69:
+    print("B")
+
+
+elif score >= 50 and score >= 59:
+    print("B-")
+
+elif score >= 40 and score >= 49:
+    print("C+")
+
+elif score >= 30 and score >= 39:
+    print("C")
+
+
+elif score >= 20 and score >= 29:
+    print("C-")
+
+
+elif score >= 10 and score >= 19:
+    print("D")
+
+
+elif score >= 0 and score >= 9:
+    print("E")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ number functions.py
@@ -0,0 +1,16 @@
+import math
+
+x=18904
+
+print(math.sqrt(x))
+print(math.pi)
+print(math.floor(78.88))
+print(math.ceil(78.15))
+
+
+
+
+
+
+
+
