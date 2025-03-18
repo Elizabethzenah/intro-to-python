fcfa92d6fdc97369bd4bd88bad948ec3a53f195d
elizabeth zenah <yienyazenha@gmail.com>
2025-03-18 10:46:01 +0300

    added python files

--- number functions.py
+++ number functions.py
@@ -7,8 +7,26 @@ print(math.pi)
 print(math.floor(78.88))
 print(math.ceil(78.15))
 
+radius=54
+height=16
 
 
+volume=22/7 *radius *radius *height
+print(volume)
+volume=round(volume,2)
+print(volume)
+
+radius=54
+height=25
+
+
+#2 *pi *r * (r+h)
+sa =22/7 *radius *radius *height
+print(sa)
+sa=round(sa,2)
+
+
+result= 10 + 20 /5 + 6 - 3 * (5+8)
 
 
 
