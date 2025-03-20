9d9c65509a5cb452086226d32f8e66a089bae634
elizabeth zenah <yienyazenha@gmail.com>
2025-03-20 11:08:33 +0300

    added python files

---
+++ functions.py
@@ -0,0 +1,20 @@
+
+
+print(34)
+x= round(67.68) # returned the result
+print(x)
+print(round(20.99))
+
+# int, float, string, list, dictionary, boolean
+# params
+# use a function == call a function
+#data == parameters
+# return value
+
+W: input("enter your weight in kgs")
+H: input("enter your height in meters")
+
+
+
+
+
